3c7675410eeb8d7068ca829704a99a7410e1215f
vitinho70777 <vitorvvmm16c@gmail.com>
2025-03-14 09:41:21 -0300

    projeto2f

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import { Component } from "react";
+class App extends Component{ 
+
+render(){
+
+return (
+
+<div>
+<h3>Aula react vite</h3>
+
+<p>Retomada de conteúdo</p>
+
+ </div>
+
+  )
+
+}
+
+}
+
+export default App
